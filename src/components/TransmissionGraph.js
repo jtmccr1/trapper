06f1305c5241784a992a52233575c255249e14a2
JT <mccrone@umich.edu>
2019-02-19 12:23:44 +0000

    force directed transmission graph

--- src/components/TransmissionGraph.js
+++ src/components/TransmissionGraph.js
@@ -26,42 +26,81 @@ class TransmissionGraph extends React.Component {
 
 		const layout = new TranmissionLayout(this.props.data);
 		layout.layOutNodes(d=>d.metaData['Date of onset']);
+
+
+
 		// layout.layOutNodes(d=>d.dateOfSampleing);
 		const xScale=d3
 					.scaleLinear()
-					.domain(d3.extent(layout.nodes,d=>d.x))
+					.domain(d3.extent(layout.nodes,d=>d.height))
 					.range([this.props.margin.left, width - this.props.margin.left - this.props.margin.right])
 					.nice()
 		const yScale = d3
 					.scaleLinear()
-					.domain(d3.extent(layout.nodes,d=>d.y))
+					.domain(d3.extent(layout.nodes,d=>d.spacingDimension))
 					.range([height - this.props.margin.top - this.props.margin.bottom, this.props.margin.bottom])
 					.nice();
+		const nodeRadius=4;
+		const force = d3.forceSimulation(layout.nodes)
+						.force("collide",d3.forceCollide(d=>nodeRadius))
+						.force("xPosition",d3.forceX(d=>xScale(d.height)).strength(0.5))
+						.force("yPosition",d3.forceY(d=>yScale(d.spacingDimension)).strength(0.5))
+						.force("center", d3.forceCenter().x(width / 2).y(height / 2));
 
-
-		svgGroup.append('g')
-				.selectAll("circle")
-				.data(layout.nodes)
-				.enter()
-				.append('circle')
-				.attr("class","caseNode")
-				.attr("r",4)
-				.attr("cx",d=>xScale(d.x))
-				.attr("cy",d=>yScale(d.y))
-		
-
-		svgGroup.append('g')
+	
+		const links = svgGroup.append('g')
 				.selectAll("line")
 				.data(layout.edges)
 				.enter()
 				.append('line')
-    			.style("stroke", "black")  // colour the line
-    			.attr("x1", d=>xScale(d.source.x))     // x position of the first end of the line
-    			.attr("y1", d=>yScale(d.source.y))      // y position of the first end of the line
-    			.attr("x2", d=>xScale(d.target.x))     // x position of the second end of the line
-   				 .attr("y2", d=>yScale(d.target.y));    // y position of the seco
+    			.style("stroke", "#ccc")  // colour the line
+				.style("stroke-width",1)
+		
+		const nodes = svgGroup.append('g')
+			.selectAll('cirlce')
+			.data(layout.nodes)
+			.enter()
+			.append("circle")
+			.attr("r",nodeRadius)
+			.style("fill","#001D73")
+			.style("stroke", "#fff")
+			.style("stroke-width", "1.5px")
+			.call(d3.drag()
+          .on("start", dragstarted)
+          .on("drag", dragged)
+          .on("end", dragended));
 
-		console.log(layout)
+			force.on("tick",()=>{
+				links.attr("x1",d=>d.source.x)
+					.attr("y1",d=>d.source.y)
+					.attr("x2",d=>d.target.x)
+					.attr("y2",d=>d.target.y);
+				nodes.attr("cx",d=>d.x)
+					.attr("cy",d=>d.y)
+
+			});
+
+			function dragstarted(d) {
+				if (!d3.event.active) force.alphaTarget(1).restart();
+				d.fx = d.x;
+				d.fy = d.y;
+			  }
+			  
+			  function dragged(d) {
+				d.fx = d3.event.x;
+				 d.fy = d3.event.y;
+			  }
+			  
+			  function dragended(d) {
+				if (!d3.event.active) force.alphaTarget(0);
+				d.fx = null;
+				 d.fy = null;
+			  }
+
+
+  
+				
+
 
 		
 
--- src/utils/transmissionLayout.js
+++ src/utils/transmissionLayout.js
@@ -2,7 +2,7 @@ import * as d3 from 'd3v4';
 export class TranmissionLayout{
     constructor(graph) {
         this.graph=graph
-        this.nodes=this.graph.getNodes().map(node=>  {return{"key":node.key,"x":null,"y":null}})
+        this.nodes=this.graph.getNodes().map(node=>  {return{"key":node.key,"height":null,"spacingDimension":null}})
         this.nodeMap = new Map(this.nodes.map(node => [node.key, node]));
         this.edges = this.graph.getEdgeList().map(edge=>{
                                                         return{"key":edge.key,
@@ -24,8 +24,8 @@ export class TranmissionLayout{
     getDataEdge(key){
         return this.graph.getEdge(key);
     }
-    layOutNodes(xFunction){
-        this.nodes.forEach(node=>node.x=xFunction(this.getDataNode(node.key)))
+    layOutNodes(heigthFunction){
+        this.nodes.forEach(node=>node.height=heigthFunction(this.getDataNode(node.key)))
         // each time we go back from going fowards count an external tip.
         // set height based on that.
         const clusters = this.graph.getClusters();
@@ -34,7 +34,7 @@ export class TranmissionLayout{
             if(b.nodes.length===1&a.nodes.length>1){
                 return -1;
             }else{
-                return(d3.min(a.nodes,d=>xFunction(d))-d3.min(b.nodes,d=>xFunction(d)))
+                return(d3.min(a.nodes,d=>heigthFunction(d))-d3.min(b.nodes,d=>heigthFunction(d)))
             }
    
         });
@@ -42,22 +42,22 @@ export class TranmissionLayout{
         for(const cluster of clusters){
             for(const externalNode of cluster.externalNodes){
                 const plotNode=this.getNode(externalNode.key)//plot node
-                plotNode.y=i;
+                plotNode.spacingDimension=i;
                 i++;
             }
         }
         // fix overplotting
         // Set the y of internal nodes as the mean of their children.
-        this.nodes.forEach(node=>this.setY(node));
+        this.nodes.forEach(node=>this.setSpacingDimension(node));
     }
 
-    setY(node){
-        if(node.y){
+    setSpacingDimension(node){
+        if(node.spacingDimension){
             return;
         }else{
             const children = this.getOutgoingEdges(node).map(edge=>edge.target);
-            children.forEach(child=>this.setY(child))
-            node.y=d3.mean(children,child=>child.y)
+            children.forEach(child=>this.setSpacingDimension(child))
+            node.spacingDimension=d3.mean(children,child=>child.spacingDimension)
         }
     }
 
