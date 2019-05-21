eaf14b587f23d0687f0283d8706d4fa757ff2cbf
JT <mccrone@umich.edu>
2019-05-21 11:04:43 +0100

    cleans up css and timeline box is the full page

--- src/components/ChartContainer.js
+++ src/components/ChartContainer.js
@@ -1,16 +1,30 @@
-import React, {useState} from 'react';
+import React, {useState,useCallback} from 'react';
 import bins from '../examples/dev/bins'
 import Chart from "./Chart"
 function ChartContainer(props){
 
-    return(
-        <div className = "timelineContainer">
-            <Chart  />
+   const [domRect,setDomRect]=useState(0);
 
+    const measuredRef = useCallback(node => {
+        if (node !== null) {
+            // setDomRect({...node.getBoundingClientRect()})
+            console.log(node.getBoundingClientRect())
+            const handleResize = () =>  {
+                setDomRect(node.getBoundingClientRect());
+
+            }
+            window.addEventListener('resize', handleResize);
+            return () => {
+              window.removeEventListener('resize', handleResize);
+            };
+        }
+      });
+    return(
+        <div className = "timelineContainer" ref={measuredRef}>
+            
         </div>
     )
-
-
+    // <Chart  />
 }
 
 export default  ChartContainer;
