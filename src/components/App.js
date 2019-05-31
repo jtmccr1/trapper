385a559ecb8d1b296ee3928a35233b0a31c4afd0
rambaut <rambaut@gmail.com>
2019-05-31 12:34:57 +0100

    Second stab at the map...

--- src/components/App.js
+++ src/components/App.js
@@ -7,93 +7,96 @@ import "../styles/trapper.css"
 
 function App() {
 
-	const [optionsOpen,setOptionsOpen] = useState(false);
-	const [sideBarOpen,setSideBarOpen] = useState(false);
-	const [timelineSize,getTimelineSize] = useState(null);
-	const [sideBarFocus,setSideBarFocus] = useState("Geography");
-	//Getting the size of the container to pass to children
-	// const [handleResize,setHandelResize] = useState(null);
-	// const otherRef = useRef(null);
-	const measuredRef = useCallback(node => {
+    const [optionsOpen,setOptionsOpen] = useState(false);
+    const [sideBarOpen,setSideBarOpen] = useState(false);
+    const [timelineSize,getTimelineSize] = useState(null);
+    const [sideBarFocus,setSideBarFocus] = useState("Geography");
+    //Getting the size of the container to pass to children
+    // const [handleResize,setHandelResize] = useState(null);
+    // const otherRef = useRef(null);
+    const measuredRef = useCallback(node => {
         if (node !== null) {
-			getTimelineSize({"height":node.getBoundingClientRect().height,"width":node.getBoundingClientRect().width})
-			const handleResize = () =>  {
-              let resizeTimer;
-              clearTimeout(resizeTimer);
-              resizeTimer = setTimeout(function() {
+            getTimelineSize({"height":node.getBoundingClientRect().height,"width":node.getBoundingClientRect().width})
+            const handleResize = () =>  {
+                let resizeTimer;
+                clearTimeout(resizeTimer);
+                resizeTimer = setTimeout(function() {
 
-              // Run code here, resizing has "stopped"
-              getTimelineSize({"height":node.getBoundingClientRect().height,"width":node.getBoundingClientRect().width});
-              }, 250);
-			}
-			
+                    // Run code here, resizing has "stopped"
+                    getTimelineSize({"height":node.getBoundingClientRect().height,"width":node.getBoundingClientRect().width});
+                }, 250);
+            }
+
 
             window.addEventListener('resize', handleResize);
             return () => {
-              window.removeEventListener('resize', handleResize);
+                window.removeEventListener('resize', handleResize);
             };
         }
-	  },[]);
+    },[]);
 
-	const getSizeAgain=()=>{
-		let resizeTimer;
-		clearTimeout(resizeTimer);
-		resizeTimer = setTimeout(function() {
-		window.dispatchEvent(new Event('resize'))
-		},0); // resize takes 0.4s
-	};
-	
-	  
-	  
-		return(
-			<div className="fillHorizontalSpace">
-			<Header />
-				<div className="mainScreen">
-					<div className={`sidebar left ${optionsOpen? "open":''}`}>
-					</div>
-					<div className="sidebarButtonColumn">
-						<div className="sidebarButtons left">
-							<div className="button" onClick={()=> {setOptionsOpen(!optionsOpen);getSizeAgain();}}>
-								 <h3>Options</h3>
-							</div>
-						</div>
-					</div>
-					<ChartContainer  ref = {measuredRef} timelineSize = {timelineSize}/>
-					<div className="sidebarButtonColumn">
-						<div className="sidebarButtons right">
-							<div className="button" onClick={()=> {
-																											if(sideBarOpen&&sideBarFocus!=="Geography"){
-																												setSideBarFocus("Geography")
-																											}else{
-																												setSideBarOpen(!sideBarOpen)
-																												setSideBarFocus("Geography")
-																											}
-																											getSizeAgain();
-																											}}>
-								 <h3>Map</h3>
-							</div>
-							<div className="button" onClick={()=> 	{
-																											if(sideBarOpen&&sideBarFocus!=="LineList"){
-																												setSideBarFocus("LineList")
-																											}else{
-																												setSideBarOpen(!sideBarOpen)
-																												setSideBarFocus("LineList")
-																											}
-																											getSizeAgain();}}>
-								 <h3>Line List</h3>
-							</div>
-						</div>
-					</div>
-					<div className={`sidebar right ${sideBarOpen? "open":''}`}>
-					{sideBarFocus==="Geography"?
-                        <div className = "geographyContainer">
-                            <Geography data={mapTopoJSON} margins={margins} size={{height: "500px", width: "500px"}} />
+    const getSizeAgain=()=>{
+        let resizeTimer;
+        clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(function() {
+            window.dispatchEvent(new Event('resize'))
+        },0); // resize takes 0.4s
+    };
+
+
+    const mapMargins = {"top":5,"bottom":5,"left":5,"right":5};
+    const mapSize = {"height": "500px", "width": "500px"};
+	const mapTopoJSON = null; // ----> need to get this from the server
+
+    return(
+        <div className="fillHorizontalSpace">
+            <Header />
+            <div className="mainScreen">
+                <div className={`sidebar left ${optionsOpen? "open":''}`}>
+                </div>
+                <div className="sidebarButtonColumn">
+                    <div className="sidebarButtons left">
+                        <div className="button" onClick={()=> {setOptionsOpen(!optionsOpen);getSizeAgain();}}>
+                            <h3>Options</h3>
                         </div>
-						:<LineList/>}
-					</div>
-				</div>
-			</div>
-			)
+                    </div>
+                </div>
+                <ChartContainer  ref = {measuredRef} timelineSize = {timelineSize}/>
+                <div className="sidebarButtonColumn">
+                    <div className="sidebarButtons right">
+                        <div className="button" onClick={()=> {
+                            if(sideBarOpen&&sideBarFocus!=="Geography"){
+                                setSideBarFocus("Geography")
+                            }else{
+                                setSideBarOpen(!sideBarOpen)
+                                setSideBarFocus("Geography")
+                            }
+                            getSizeAgain();
+                        }}>
+                            <h3>Map</h3>
+                        </div>
+                        <div className="button" onClick={()=> 	{
+                            if(sideBarOpen&&sideBarFocus!=="LineList"){
+                                setSideBarFocus("LineList")
+                            }else{
+                                setSideBarOpen(!sideBarOpen)
+                                setSideBarFocus("LineList")
+                            }
+                            getSizeAgain();}}>
+                            <h3>Line List</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className={`sidebar right ${sideBarOpen? "open":''}`}>
+                    {sideBarFocus==="Geography"?
+                        <div className = "geographyContainer">
+                            <Geography data={mapTopoJSON} margins={mapMargins} size={mapSize} />
+                        </div>
+                        :<LineList/>}
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default App;
--- src/components/Geography.js
+++ src/components/Geography.js
@@ -1,6 +1,7 @@
-import React, {useCallback, useState} from 'react';
-import 'react-table/react-table.css';
-import {select} from "d3-selection";
+import React, {props, useRef, useState} from 'react';
+import {select} from "d3";
+import {geoEqualEarth, geoPath} from "d3-geo";
+import {feature} from "topojson";
 
 function Geography(){
     const [map, setMap] = useState(null);
@@ -10,62 +11,62 @@ function Geography(){
     const mapData = props.data;
 
     // center on UK
-    const projection = d3.geo.mercator()
+    const projection = geoEqualEarth()
         .center([3.4360, 55.3781])
         .scale(1000)
         .translate([width / 2, height / 2]);
 
-    const path = d3.geo.path()
+    const path = geoPath()
         .projection(projection);
 
-    const zoom = d3.behavior.zoom()
-        .translate(projection.translate())
-        .scale(projection.scale())
-        .scaleExtent([height, 8 * height])
-        .on("zoom", zoomed);
+    // const zoom = d3.behavior.zoom()
+    //     .translate(projection.translate())
+    //     .scale(projection.scale())
+    //     .scaleExtent([height, 8 * height])
+    //     .on("zoom", zoomed);
 
-    const adm0 = topojson.feature(mapData, mapData.objects.adm0);
-    const adm1 = topojson.feature(mapData, mapData.objects.adm1);
+    const adm0 = feature(mapData, mapData.objects.adm0);
+    const adm1 = feature(mapData, mapData.objects.adm1);
 
-    const el = useCallback(svgNode => {
+    const el = useRef();
 
-        if (svgNode !== null) {
+    if (el.current !== null) {
 
-            if(svgNode.children.length===0) { // make it the first time
+        if (el.current.children.length === 0) { // make it the first time
 
-                //to selecting every time
-                const svg = select(svgNode);
+            //to selecting every time
+            const svg = select(el.current);
 
-                const g = svg.append("g")
-                    .call(zoom);
+            const g = svg.append("g");
+            // .call(zoom);
 
-                g.append("rect")
-                    .attr("class", "background")
-                    .attr("width", width)
-                    .attr("height", height);
+            g.append("rect")
+                .attr("class", "background")
+                .attr("width", width)
+                .attr("height", height);
 
-                g.append("g")
-                    .attr("id", "adm1")
-                    .selectAll("path")
-                    .data(adm1.features)
-                    .enter().append("path")
-                    .attr("class", "adm1")
-                    .attr("id", (d) => d.properties.name)
-                    .attr("d", path);
+            g.append("g")
+                .attr("id", "adm1")
+                .selectAll("path")
+                .data(adm1.features)
+                .enter().append("path")
+                .attr("class", "adm1")
+                .attr("id", (d) => d.properties.name)
+                .attr("d", path);
 
-                g.append("g")
-                    .attr("id", "adm0")
-                    .selectAll("path")
-                    .data(adm0.features)
-                    .enter().append("path")
-                    .attr("class", "adm0")
-                    .attr("id", (d) => d.properties.name)
-                    .attr("d", path);
+            g.append("g")
+                .attr("id", "adm0")
+                .selectAll("path")
+                .data(adm0.features)
+                .enter().append("path")
+                .attr("class", "adm0")
+                .attr("id", (d) => d.properties.name)
+                .attr("d", path);
 
             // }else{
             //     update?
-            }
-        }});
+        }
+    }
 
     return(
         <svg className="chart" id={"map"}
