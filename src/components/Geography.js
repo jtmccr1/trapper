85c6b685ed6c1098967d76877e209fc6683344e9
JT <mccrone@umich.edu>
2019-05-31 10:58:07 +0100

    add linelist and geography place holder components

---
+++ src/components/Geography.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
+
+
+function Geography(){
+       
+        return (
+            <h1 style={{color:'black'}}>Look at this Map!</h1>
+)
+}
+
+export default Geography;
